f5c6b843cf1c2d996af3f28f3c3bae67e8f8e0e3
Hakan Dilek <hakandilek@gmail.com>
2015-04-03 20:22:26 +0200

    initial Android Studio app with AndroidAnnotations support

---
+++ app/src/main/java/me/dilek/izlek/IzlekApplication.java
@@ -0,0 +1,13 @@
+package me.dilek.izlek;
+
+import android.app.Application;
+
+import org.androidannotations.annotations.EApplication;
+
+/**
+ * Android Application
+ * Created by Hakan Dilek on 03.04.15.
+ */
+@EApplication
+public class IzlekApplication extends Application {
+}
