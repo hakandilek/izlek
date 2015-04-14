fb2896d170b81dd7f8e19501ab3a9e8c18f3721a
Hakan Dilek <hakandilek@gmail.com>
2015-04-15 00:04:04 +0200

    introduce cezmi dependency with cling dependency for dlna

---
+++ app/src/main/java/me/dilek/izlek/cezmi/CezmiHolder.java
@@ -0,0 +1,15 @@
+package me.dilek.izlek.cezmi;
+
+import org.androidannotations.annotations.EBean;
+
+import me.dilek.cezmi.Cezmi;
+
+/**
+ * Created by Hakan Dilek on 14.04.15.
+ */
+@EBean
+public class CezmiHolder {
+
+    Cezmi cezmi = new Cezmi();
+
+}
---
+++ cezmi/src/main/java/me/dilek/cezmi/dlna/ServerLocator.java
@@ -0,0 +1,7 @@
+package me.dilek.cezmi.dlna;
+
+/**
+ * Created by Hakan Dilek on 14.04.15.
+ */
+public class ServerLocator {
+}
